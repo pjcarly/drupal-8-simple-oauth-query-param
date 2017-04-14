85f84b0c6bbda2625f3cc4ac3f35200b16a9cc3a
Pieterjan C <pieterjan.carly@gmail.com>
2017-04-14 15:45:25 +0200

    space

--- src/Authentication/Provider/SimpleOauthQueryParamAuthenticationProvider.php
+++ src/Authentication/Provider/SimpleOauthQueryParamAuthenticationProvider.php
@@ -21,15 +21,16 @@ use Symfony\Component\HttpFoundation\Request;
 class SimpleOauthQueryParamAuthenticationProvider extends SimpleOauthAuthenticationProvider
 {
   public static function getTokenValue(Request $request)
-  {    
+  {
     // Implementation of http://tools.ietf.org/html/rfc6750#section-2.3 as an addon for simple_auth
     // Please be considerate of the security implications, only use this in cases where
     // an Authorization Header cannot be passed
+
     if($request->query->has('access_token'))
     {
       return $request->query->get('access_token');
     }
-    
+
     return NULL;
   }
 }
